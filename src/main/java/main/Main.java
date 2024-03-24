77408d1f8821eb47f0eceacac4282b5ed99cd14e
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-03-24 12:10:30 +0100

    Leetcode

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,16 @@
 package main;
 
+import org.example.ClimbStairs;
+import org.example.SingleNum;
+import org.example.Sqrt;
 import org.example.solution;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(solution.containsDuplicate(new int[]{1,2,1,3}));
-        System.out.println(solution.containsDuplicate(new int[]{2,3,4,5,6,7}));
-        System.out.println(solution.containsDuplicate(new int[]{1,1,2,3,4}));
-        System.out.println(solution.containsDuplicate(new int[]{2,3,5,6,2}));
-        System.out.println(solution.containsDuplicate(new int[]{1,2,3,6,7,2}));
+        System.out.println(ClimbStairs.climbStairs(12));
+        System.out.println(SingleNum.singleNumber(new int[]{1, 2, 3, 4}));
               }
           }
 
