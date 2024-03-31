f467ca4015dc70e2d7d44204acdfa6911bf88c87
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-03-31 18:06:51 +0200

    Leetcode

---
+++ src/main/java/org/example/SortColors.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public void SortColors(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
