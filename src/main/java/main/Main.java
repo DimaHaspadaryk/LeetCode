a349b3098a6784406b5bdb0f725f9f65fffa155f
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-04-17 15:35:39 +0200

    Leetcode

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,22 @@
 package main;
 
-import org.example.ClimbStairs;
-import org.example.SingleNum;
-import org.example.Sqrt;
-import org.example.solution;
+import org.example.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ClimbStairs.climbStairs(12));
-        System.out.println(SingleNum.singleNumber(new int[]{1, 2, 3, 4}));
-              }
-          }
+      Revers revers = new Revers();
+        System.out.println(revers.reverse(123));
+        System.out.println(revers.reverse(-123));
+        System.out.println(revers.reverse(11112222));
 
+
+
+
+
+
+
+
+    }
+}
