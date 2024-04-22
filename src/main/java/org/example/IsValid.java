d20a99f30fc99a85b406c9b6108193d4abbfafa0
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-04-22 15:29:05 +0200

    Leetcode

---
+++ src/main/java/org/example/IsValid.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Stack;
+
+public class IsValid {
+    public boolean isValid(String s) {
+        boolean check = false;
+        Stack<Character> stack = new Stack<>();
+
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
