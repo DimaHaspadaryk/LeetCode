7f9296a77453e498485682a8b20a6e9565d5de03
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-05-09 13:54:18 +0200

    Leetcode

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,13 +2,17 @@ package main;
 
 import org.example.*;
 
-import java.util.Arrays;
+
+
 
 public class Main {
     public static void main(String[] args) {
-     RemoveDuplicateLetters removeDuplicateLetters =  new RemoveDuplicateLetters();
-        System.out.println(removeDuplicateLetters.removeDuplicateLetters("bcaaabc"));
-        System.out.println(removeDuplicateLetters.removeDuplicateLetters("adsadsadas"));
+    MaxProfit maxProfit = new MaxProfit();
+        System.out.println(maxProfit.maxProfit(new int[]{1,2,3,6}));
+        System.out.println(maxProfit.maxProfit(new int[]{4,3,2,1}));
+        System.out.println(maxProfit.maxProfit(new int[]{1,1,2,3,6,7}));
+        System.out.println(maxProfit.maxProfit(new int[]{0,0,2,3,1,0}));
+
 
 
 
---
+++ src/main/java/org/example/MaxProfit.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = prices[0];
+
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+            else if (prices[i] - buy > profit) {
+                profit= prices[i] - buy;
+            }
+        }
+
+        return profit;
+    }
+}
