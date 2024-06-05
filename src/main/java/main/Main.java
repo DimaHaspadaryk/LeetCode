a7a88472dee0691aa74576e1bc622e71715a3d7f
Dimasiys228 <dimagaspodarik98@gmail.com>
2024-06-05 17:47:00 +0200

    Leetcode

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,23 +3,13 @@ package main;
 import org.example.*;
 
 
-
-
 public class Main {
     public static void main(String[] args) {
-    MaxProfit maxProfit = new MaxProfit();
-        System.out.println(maxProfit.maxProfit(new int[]{1,2,3,6}));
-        System.out.println(maxProfit.maxProfit(new int[]{4,3,2,1}));
-        System.out.println(maxProfit.maxProfit(new int[]{1,1,2,3,6,7}));
-        System.out.println(maxProfit.maxProfit(new int[]{0,0,2,3,1,0}));
+GoodPairs goodPairs = new GoodPairs();
+        System.out.println(goodPairs.numIdenticalPairs(new int[]{1,2,3,1,1,3}));
+        System.out.println(goodPairs.numIdenticalPairs(new int[]{1,1,1,1}));
+
+            }
+        }
 
 
-
-
-
-
-
-
-
-    }
-}
